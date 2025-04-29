58947f8717131c45c77a2aac41d53ddfa8cdb816
Camila Celente. <camilacelente@yahoo.com.br>
2025-04-29 00:56:10 -0300

    Create Introdução as Strings.py

---
+++ Introdução as Strings.py
@@ -0,0 +1,18 @@
+
+#introducaoaistrings.py
+
+nome = "Camila Celente Eggert"
+type(nome)
+nome2 = 'Camila Celente Eggert'
+nome3 = '''
+Esta é uma string longa
+contendo informações sobre Python
+'''
+nome4 = """
+Esta é outra string longa
+contendo informações sobre Python
+"""
+print(nome)
+print(nome2)
+print(nome3)
+print(nome4)
