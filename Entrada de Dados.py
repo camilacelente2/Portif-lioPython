6019a7a97bbb7278784cd805a52ecb619b961ed7
Camila Celente. <camilacelente@yahoo.com.br>
2025-04-29 01:03:34 -0300

    Create Entrada de Dados.py

---
+++ Entrada de Dados.py
@@ -0,0 +1,10 @@
+
+#entrada de dados.
+
+nome = input("Escreva seu nome: ")#String
+idade = int(input("Escreva sua idade: "))#Inteiro
+peso = float(input("Escreva seu peso: "))#float
+
+print(nome)
+print(idade)
+print(peso)
