8c72029228979681f621168d3062aa1f7a9a7c0c
Camila Celente. <camilacelente@yahoo.com.br>
2025-08-14 10:54:53 -0300

    Create decisões simples.py

---
+++ decisões simples.py
@@ -0,0 +1,14 @@
+var_n1 = 0
+var_n2 = 0
+var_media = 0
+
+msg = "Aprovado"
+
+var_n1 = input("Digite a 1a nota ")
+var_n2 = input("Digite a 2a nota ")
+
+var_media = ( int (var_n1) + int (var_n2 ) ) / 2
+
+
+if (var_media >=7):
+  print (msg)
