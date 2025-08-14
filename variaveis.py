965a306ac4f9f84f83a1d7d297d7796a7e5bf5d1
Camila Celente. <camilacelente@yahoo.com.br>
2025-08-14 10:33:00 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,14 @@
+var_nome = ""
+var_n1 = 0
+var_n2 = 0
+var_media = 0
+
+
+var_nome = input ("Seu nome ")
+var_n1 = input ("Primeira nota ")
+var_n2 = input ("Segunda nome ")
+
+var_media = (int (var_n1) + int (var_n2)) / 2
+
+
+print (var_media)
