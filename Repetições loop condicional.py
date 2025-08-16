1bf49b9cb81a7647d745350e72a32dec5c00f452
Camila Celente. <camilacelente@yahoo.com.br>
2025-08-16 17:35:02 -0300

    Create Repetições loop condicional.py

---
+++ Repetições loop condicional.py
@@ -0,0 +1,18 @@
+var_capital = 0
+var_capital_apli = 0
+var_taxa = 0
+var_meses = 0
+var_contador = 1
+
+var_capital = input("Capital de entrada ")
+var_capital_apli = float(var_capital)
+var_taxa = input("Taxa de rentabilidade ao mês ")
+var_meses = input("Meses de aplicacao ")
+
+while int (var_contador) <= int (var_meses):
+ var_capital_apli = float(var_capital_apli) + (float(var_capital_apli) *  float(var_taxa))
+ print(var_contador)
+ var_contador = var_contador + 1
+
+print("Capital de entrada ", var_capital)
+print("Capital final ", var_capital_apli)
